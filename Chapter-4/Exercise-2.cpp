ed1460d2b9cbbabbeb67173360714e17d4d2c987
Eunice Oh <euniceohsj@gmail.com>
2020-07-06 19:16:02 -0700

    Create Exercise-2.cpp

---
+++ Chapter-4/Exercise-2.cpp
@@ -0,0 +1,38 @@
+/*
+create a function substring that takes three parameters: an arrayString, a starting position integer, and an integer length of characters. 
+The function returns a pointer to a new dynamically allocated string array.
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+typedef char * arrayString; //pointer to an array of characters in HEAP
+
+/*
+ returns: a pointer to new dynamically allocated string array
+ original string is unaffected
+*/
+arrayString substring(arrayString s, int start, int length){
+    arrayString newS = new char[length+1];
+    
+    for(int i = 0; i < length; i++){
+        newS[i] = s[start+i];
+    }
+    newS[length] = 0;
+    
+    return newS;
+}
+
+int main() {
+    
+    //test case 3:
+    arrayString e = new char[7];
+    e[0] = 'e'; e[1] = 'u'; e[2] = 'n'; e[3] = 'i'; e[4] = 'c'; e[5] = 'e'; e[6] = 0;
+    
+    arrayString newE = substring(e, 2, 4);
+    
+    cout << newE << endl << e << endl;
+
+    return 0;
+}
