2a741e6b0614d1d8d59b062b1f1f63d82d6dac8f
Eunice Oh <euniceohsj@gmail.com>
2020-07-19 23:21:24 -0700

    Update pointers.cpp

--- Chapter-4/pointers.cpp
+++ Chapter-4/pointers.cpp
@@ -6,24 +6,45 @@ int main() {
      Pointers
      */
     
-    int* applePointer; // an int pointer, a type that holds memory address of integers
-    int apple = 8;     // a variable with an integer type
+    int* applePointer;              // an int pointer, a type that holds memory address of integers
+    int apple = 8;                  // a variable with an integer type
     
-    applePointer = &apple;  // applePointer contains memory address of apple
-    //applePointer now points to apple
+    applePointer = &apple;          // applePointer contains memory address of apple
+                                    //applePointer now points to apple
     cout << "Address of apple: " << applePointer << endl;
     
-    int appleValue;    // a variable with the same type as apple
-    appleValue = *applePointer; // dereferencing apple pointer
-    // accessing memory at other end of applePointer
+    int appleValue;                 // a variable with the same type as apple
+    appleValue = *applePointer;     // dereferencing apple pointer
+                                    // accessing memory at other end of applePointer
     cout << "Value of apple: " << appleValue << endl;
     
     /*
      References
      */
     
-    int& appleReference = apple; // both apple & appleReference can access the value of apple
+    int& appleReference = apple;    // both apple & appleReference can access the value of apple
     applePointer = &appleReference; // applePointer points to memory address of apple
     cout << "Address of appleReference: " << applePointer << endl;
     
+    /*
+    Pointers and Arrays
+    */
+    
+    int numbers[5];
+    int * p;
+    p = numbers;
+    *p = 10;
+    p++;
+    *p = 20;
+    p = &numbers[2];
+    *p = 30;
+    p = numbers + 3;
+    *p = 40;
+    p = numbers;
+    *(p+4) = 50; // same thing as p[4]
+    
+    for (int n=0; n<5; n++)
+      cout << numbers[n] << ", ";
+    
+    return 0; 
 }
